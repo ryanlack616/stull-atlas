fd2a0f8eed2620b3f2a9cd071924efe539228cfa
Ryan <rlackpotter@gmail.com>
2026-02-09 00:49:41 -0500

    chore: remove scratch scripts

--- list_titles.py
+++
@@ -1,9 +0,0 @@
-"""List all event titles with their exact characters."""
-import re
-
-with open(r'src\pages\timelineData.ts', 'r', encoding='utf-8') as f:
-    content = f.read()
-
-titles = re.findall(r"title: '((?:[^'\\]|\\.)*)',", content)
-for t in titles:
-    print(repr(t))
--- list_titles2.py
+++
@@ -1,6 +0,0 @@
-import re
-with open('src/pages/timelineData.ts', 'r', encoding='utf-8') as f:
-    content = f.read()
-titles = re.findall(r"title: '((?:[^'\\\\]|\\\\.)*)',", content)
-for t in titles:
-    print(repr(t))
